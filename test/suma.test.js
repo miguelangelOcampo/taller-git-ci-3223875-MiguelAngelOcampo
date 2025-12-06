5f4fdc7050b9e898015f9c9627aff1993c680236
Miguel Angel Ocampo Muñoz <miguelangelocampo713@gmail.com>
2025-12-06 14:39:06 -0500

    feat:Instalación de DOM y modificacion a test para la nueva función

--- test/suma.test.js
+++ test/suma.test.js
@@ -1,5 +1,22 @@
-const sumar = require("../src/app");
+/**
+ * @jest-environment jsdom
+ */
 
-test('la función sumar debe devolver la suma de dos números', () => {
+const { sumar, agregarItem } = require("../src/app.js");
+
+test("la función sumar debe devolver la suma de dos números", () => {
   expect(sumar(2, 3)).toBe(5);
+});
+
+test("agregarItem debe agregar un <li> a la lista", () => {
+  document.body.innerHTML = `
+    <ul id="lista"></ul>
+  `;
+
+  agregarItem("Hola SENA");
+
+  const lista = document.getElementById("lista");
+
+  expect(lista.children.length).toBe(1);
+  expect(lista.children[0].textContent).toBe("Hola SENA");
 });
